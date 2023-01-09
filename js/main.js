6f7dd0ae1e98ed1948091fabce89d8648a76598e
Ethan <ethangroene@gmail.com>
2023-01-09 16:54:06 -0600

    Populate char-info & quote cards from same source
    Each type of card has its own func to populate it

--- js/main.js
+++ js/main.js
@@ -6,45 +6,22 @@ const allChars = [
     { name: 'Badger', imageURL: './assets/badger.jpg', quote: 'Darth Vader had responsibilities. He was responsible for the Death Star.'},
 ]
 
-// Function to put chars' data (name, img) into array
-// This will be used to populate the cards containing each char's info (name, img)
-const charsInfo = [];
-function charsInfoIntoArray() {
-    for (let char of allChars) {
-        let charInfoObject = { name: char.name, imageURL: char.imageURL };
-        charsInfo.push(charInfoObject);
-    }
-}
-charsInfoIntoArray();
-console.log(charsInfo);
-
 // Function to display cards containing each char's image and name:
 // Add data-name to each char card:
-function displayCharsData() {
-    for (let char of charsInfo) {
-        cardArea.innerHTML += "<div class='char-card-data' data-name='" + char.name.toLowerCase().replace(/ /g, '-') + "'>"
+function displayCharsNameImage() {
+    for (let char of allChars) {
+        cardArea.innerHTML += "<div class='info-card' data-name='" + char.name.toLowerCase().replace(/ /g, '-') + "'>"
         + "<img src='" + char.imageURL + "'>"
         + "<p>" + char.name + "</p>"
         + "</div>"
     }
 }
-displayCharsData();
+displayCharsNameImage();
 
-// Function to put each char's quote into array
-// This will be used to populate the quote cards
-const charQuotes = [];
-function charQuoteIntoArray() {
-    for (let char of allChars) {
-        charQuotes.push(char.quote);
-    }
-}
-charQuoteIntoArray();
-console.log(charQuotes);
-
-// Function to display cards containing char's quote:
-// Add data-name to each char card:
+// Function to display cards containing each char's quote:
+// Add data-name to each quote card:
 function displayCharsQuote() {
-    for (let char of charsQuotes) {
+    for (let char of allChars) {
         cardArea.innerHTML += "<div class='quote-card' data-name='" + char.name.toLowerCase().replace(/ /g, '-') + "'>"
         + "<p>" + char.quote + "</p>"
         + "</div>"
