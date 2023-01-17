b010c9981271840b864fc27e68e52bbc69654977
Ethan <ethangroene@gmail.com>
2023-01-17 01:26:20 -0600

    Increase display time of result upon match

--- js/main.js
+++ js/main.js
@@ -432,7 +432,7 @@ for (let btn of startBtns) {
                     setTimeout(function() {
                         selectionResultBox.style.display = 'none';
                         selectionResultBox.innerHTML = ""
-                    }, 2500);
+                    }, 3500);
 
                     // Disable current cards matched add 'matched' to each card's classlist & push to matched cards array:
                     for (let card of selectedCards) {
